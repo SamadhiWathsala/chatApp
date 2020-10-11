c267fd7d102a1e0e7204c137ba77fd7759745825
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-10-11 20:29:05 +0530

    sign up UI/UX and logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+import LoginComponent from './login/login';
+import DashboardComponent from './dashboard/dashboard';
+import SignUpComponent from './signup/signUp';
+
+
+const firebase = require ('firebase')
+require ('firebase/firestore')
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAisDiP2lr8j5CiIePs_qKJtrLzmjgUbDI",
+    authDomain: "chat-4bb7d.firebaseapp.com",
+    databaseURL: "https://chat-4bb7d.firebaseio.com",
+    projectId: "chat-4bb7d",
+    storageBucket: "chat-4bb7d.appspot.com",
+    messagingSenderId: "634251149628",
+    appId: "1:634251149628:web:83c737433b321dc2ff1e39",
+    measurementId: "G-K2WYD47M3G"
+})
+
+
+const routing = (
+
+  <Router>
+    <div id ='routing-container'>
+      <Route path='/login' component = {LoginComponent}></Route>
+      <Route path='/signUp' component = {SignUpComponent}></Route>
+      <Route path='/dashboard' component = {DashboardComponent}></Route>
+    </div>
+  </Router>
+
+)
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
